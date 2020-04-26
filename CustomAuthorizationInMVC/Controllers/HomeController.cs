4f67c91addbb6fd3b3f151a82ee02e2ce242c2fa
Cian Mung <cianmung@zomi.net>
2020-04-27 00:31:59 +0800

    first commit

---
+++ CustomAuthorizationInMVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CustomAuthorizationInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomAuthorizationInMVC.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AuthorizeRole(Role.SUPERADMIN, Role.ADMIN)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AuthorizeRole(Role.SUPERADMIN)]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+        [AuthorizeRole(Role.ADMIN)]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+        [AuthorizeRole(Role.USER)]
+        public ActionResult UserPage()
+        {
+            ViewBag.Message = "User Page";
+            return View();
+        }
+    }
+}
---
+++ CustomAuthorizationInMVC/Models/Repository.cs
@@ -0,0 +1,22 @@
+﻿using CustomAuthorizationInMVC.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomAuthorizationInMVC.Models
+{
+    public class Repository
+    {
+        public USER GetUserDetails(string UserName, string Password)
+        {
+            USER user = new USER();
+            using (UserDBContext db = new UserDBContext())
+            {
+                user = db.USERS.Where(u => u.UserName.ToLower() == UserName.ToLower() &&
+                                      u.Password == Password).FirstOrDefault();
+            }
+            return user;
+        }
+    }
+}
